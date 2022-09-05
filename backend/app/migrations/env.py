008c2afe8ade438ec960a5e7ae25591305ed68eb
Jakub Tomala <jakub.tomala@stxnext.pl>
2022-09-05 20:36:48 +0200

    [20] Linters fix

--- backend/app/migrations/env.py
+++ backend/app/migrations/env.py
@@ -3,8 +3,9 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from app.db.database import Base
+
 from ..core.config import settings
-from psychological_counselling.backend.app.db.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
