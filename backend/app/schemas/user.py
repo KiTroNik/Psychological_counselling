8b09ab9c92a10dc875ae0dd12b1fa74cffdb70a6
Jakub Tomala <jakub.tomala@stxnext.pl>
2022-09-06 14:32:05 +0200

    [21] Added user schemas

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    is_admin: bool
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
