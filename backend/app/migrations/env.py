e11d1c637b9751bd5b31639f6bd509d6529c86da
Jakub Tomala <jakub.tomala@stxnext.pl>
2022-09-06 15:36:32 +0200

    [21] Linter fixes

--- backend/app/migrations/env.py
+++ backend/app/migrations/env.py
@@ -3,10 +3,9 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from app.db.database import Base
-
 from app.core.config import settings
-from app.models import user
+from app.db.database import Base
+from app.models import user  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -14,7 +13,8 @@ config = context.config
 
 config.set_main_option(
     "sqlalchemy.url",
-    f"postgresql://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}/{settings.DB_NAME}",
+    (f"postgresql://{settings.DB_USER}:{settings.DB_PASS}"
+        f"@{settings.DB_HOST}/{settings.DB_NAME}"),
 )
 
 # Interpret the config file for Python logging.
