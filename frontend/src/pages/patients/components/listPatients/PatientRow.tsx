fed82d46a75b87514688fbaf3c8a5ac59c6c0297
Jakub Tomala <jakub.tomala@stxnext.pl>
2023-01-06 19:10:02 +0100

    [69] Added delete patient functionality.

--- frontend/src/pages/patients/components/listPatients/PatientRow.tsx
+++ frontend/src/pages/patients/components/listPatients/PatientRow.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import APP_ROUTES from "../../../../core/routes";
+import { useDeletePatient } from "../../api";
 
 interface PatientRowProps {
   id: number;
@@ -9,6 +10,8 @@ interface PatientRowProps {
 }
 
 const PatientRow = ({ id, email, first_name, last_name }: PatientRowProps) => {
+  const mutation = useDeletePatient(id);
+
   return (
     <tr>
       <td className="whitespace-no-wrap border-b border-gray-200 px-6 py-4">
@@ -44,20 +47,22 @@ const PatientRow = ({ id, email, first_name, last_name }: PatientRowProps) => {
         </Link>
       </td>
       <td className="whitespace-no-wrap border-b border-gray-200 px-6 py-4 text-sm leading-5 text-gray-500">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="h-6 w-6 text-red-400 hover:text-red-700"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-          />
-        </svg>
+        <button onClick={async () => await mutation.mutateAsync({ id: id })}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6 text-red-400 hover:text-red-700"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+            />
+          </svg>
+        </button>
       </td>
     </tr>
   );
